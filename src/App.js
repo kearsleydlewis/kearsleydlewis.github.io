4ddc4fd31ac121c812760fbc91d7c9c8852390bb
Dillon Lewis <kearsleydlewis@gmail.com>
2021-08-21 17:05:39 -0400

    Placeholder text until I upload full website.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dillon Lewis's personal website is under construction.
         </p>
         <a
           className="App-link"
